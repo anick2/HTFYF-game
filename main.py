7a58f39f06d98884098cc554cd16606976d805bc
AndiDu <andiduorange@gmail.com>
2020-04-23 00:29:45 +0300

    states updated

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ def init():
 class Game:
     def __init__(self):
         self.screen = screen
-        #self.clock = pygame.time.Clock()
-        #self.current_time = 0.0
         #self.keys = pg.key.get_pressed()   
         self.done = False                   #конец игры
         self.fps = 60
@@ -33,24 +31,19 @@ class Game:
         self.state = self.state_dict[start_state]
 
     def update(self):
-        #self.current_time = pygame.time.get_ticks()
-        if self.state.quit:
-            self.done = True
-        elif self.state.done:
+        if self.state.done:
             self.flip_state()
-        self.state.update(self.screen, self.keys, self.current_time)
+        self.state.on_update()
 
     def flip_state(self):
-        '''смена состояний'''
+        # смена состояний
         previous, self.state_name = self.state_name, self.state.next
-        persist = self.state.cleanup()
         self.state = self.state_dict[self.state_name]
-        self.state.startup(self.current_time, persist)
-        self.state.previous = previous
+        self.state.on_create()
     
     def event_loop(self):
         for event in pygame.event.get():
-            '''нужно написать обработку событий'''
+            # нужно написать обработку событий
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
@@ -62,55 +55,144 @@ class Game:
                     sys.exit()
                     self = False
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if pygame.mouse.get_pos()[0] >= 500 and pygame.mouse.get_pos()[1] >= 300:
-                    if pygame.mouse.get_pos()[0] <= 600 and pygame.mouse.get_pos()[1] <= 350:
-                        pygame.quit()
-                        sys.exit()
-                        self = False
-                      
+            self.state.get_event(event);
+            
     def start(self):
-        '''mainloop'''
+        # запуск первого состояния
+        self.state.on_create();
+        # запуск мэйнлупа
+        self.mainloop();
+        
+
+    def mainloop(self):
         while not self.done:
             self.event_loop()
-            pygame.display.flip()
-            screen.blit(IMAGES["menu"], screen_rect)
-            pygame.draw.rect(screen,(255,255,255),(500,300,100,50));
-        self.clock.tick(self.fps)
+            self.update()
+        self.clock.tick(self.fps) 
         
 
 
-class _State(object):
+class State:
     def __init__(self):
-        self.start_time = 0.0
-        self.current_time = 0.0
-        self.done = False
-        self.quit = False
-        self.next = None
-        self.previous = None
-        self.persist = {}
+        self.done     = False # состояние завершилось
+        self.next     = None  # следубщее состояние
+        self.prev     = None  # предудущее состояние
 
+    # запускается при создании
+    def on_create(self):
+        pass
+
+    # запускается @fps раз в секунду для обновления
+    def on_update(self):
+        pass
+
+    # запускается для отлова ивентов
     def get_event(self, event):
         pass
 
-    def startup(self, current_time, persistant):
-        self.persist = persistant
-        self.start_time = current_time
+    # задаем соседние состояния
+    def set_prenex(self, pre, nex):
+        self.next = nex;
+        self.prev = pre;
+    
 
-    def cleanup(self):
-        self.done = False
-        return self.persist
+# класс для состояния "меню"
+class MainMenu(State):        
+    def __init__(self):
+        State.__init__(self)
+        self.on_create();     
 
-    def update(self, surface, keys, current_time):
+    def on_create(self):
+        pygame.display.flip()
+        #изображение:
+        screen.blit(IMAGES["menu"], screen_rect)
+        pygame.draw.rect(screen,(255,255,255),(500,300,100,50));
+
+    def on_update(self):
+        pygame.display.flip()
+        screen.blit(IMAGES["menu"], screen_rect)
+        pygame.draw.rect(screen,(255,255,255),(500,300,100,50));
+
+    def get_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pos()[0] >= 500 and pygame.mouse.get_pos()[1] >= 300:
+                if pygame.mouse.get_pos()[0] <= 600 and pygame.mouse.get_pos()[1] <= 350:
+                    self.done = True
+    
+# класс для состояния "лес"
+class Forest(State):          
+    def __init__(self):
+        State.__init__(self)
+        self.on_create();   
+        
+    def on_create(self):      
+        pygame.display.flip()
+        screen.blit(IMAGES["forest"], screen_rect)
+        pygame.draw.rect(screen,(255,255,255),(600,300,100,50));
+
+    def on_update(self):
+        pygame.display.flip()
+        screen.blit(IMAGES["forest"], screen_rect)
+        pygame.draw.rect(screen,(255,255,255),(600,300,100,50));
+
+    def get_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pos()[0] >= 600 and pygame.mouse.get_pos()[1] >= 300:
+                if pygame.mouse.get_pos()[0] <= 700 and pygame.mouse.get_pos()[1] <= 350:
+                    self.done = True
+
+# класс для состояния "город"
+class City(State):          
+    def __init__(self):
+        State.__init__(self)
+        self.on_create();   
+        
+    def on_create(self):      
+        pygame.display.flip()
+        screen.blit(IMAGES["city"], screen_rect)
+        pygame.draw.rect(screen,(255,255,255),(400,300,100,50));
+
+    def on_update(self):
+        pygame.display.flip()
+        screen.blit(IMAGES["city"], screen_rect)
+        pygame.draw.rect(screen,(255,255,255),(400,300,100,50));
+
+    def get_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pos()[0] >= 400 and pygame.mouse.get_pos()[1] >= 300:
+                if pygame.mouse.get_pos()[0] <= 500 and pygame.mouse.get_pos()[1] <= 350:
+                    self.done = True
+
+class Park(State):          # класс для состояния "парк"
+    def __init__(self):
+        State.__init__(self)
+        self.on_create();   
+        
+    def on_create(self):      
+        pygame.display.flip()
+        screen.blit(IMAGES["park"], screen_rect)
+
+    def on_update(self):
+        pygame.display.flip()
+        screen.blit(IMAGES["park"], screen_rect)
+
+    def get_event(self, event):
         pass
 
 def main():
     init()
     game = Game()
-    states = {} # словаь состояний {"MAIN_MENU": MainMenu()}
-    #first_state = "MAIN_MENU"
-    #game.setup_states(states, first_state)
-    game.start()
+    states = {"MAIN_MENU": MainMenu(),
+              "FOREST": Forest(),
+              "CITY": City(),
+              "PARK": Park()}                        # словарь состояний 
+    first_state = "MAIN_MENU"                        # начальное состояние
+    states["MAIN_MENU"].set_prenex(None, "FOREST")   # объявление состояний
+    states["FOREST"].set_prenex("MAIN_MENU", "CITY") 
+    states["CITY"].set_prenex("FOREST", "PARK")
+    states["PARK"].set_prenex("PARK", None)
+    game.setup_states(states, first_state)           # установка состояний
+    game.start()                                     # начало игры
     
 if __name__ == '__main__':
     main()
