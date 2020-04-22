a2d5a269d8e3455d61c5ae3bcce8566199180b64
Yuliya <anick2704@mail.ru>
2020-04-22 05:53:02 +0300

    first look at game logic

--- main.py
+++ main.py
@@ -1,64 +1,124 @@
-import pygame
 import sys
-import os
+import pygame
+import const as c
 
-clock = pygame.time.Clock()
-pygame.init()
+def init():
+    global screen, screen_rect
+    pygame.init()
+    pygame.display.set_caption(c.CAPTION)
+    screen = pg.display.set_mode(c.SCREEN_SIZE)
+    screen_rect = screen.get_rect()
 
-worldx = 800
-worldy = 336
-
-world       = pygame.display.set_mode([worldx,worldy])
-backdrop    = pygame.image.load(os.path.join('nat.png')).convert()
-backdropbox = world.get_rect()
-
-'''
-Objects
-'''
-
-class Player(pygame.sprite.Sprite):
+class Game:
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.images = []
-        img = pygame.image.load('hero.png').convert()
-        img.convert_alpha()     # optimise alpha
-        img.set_colorkey((255,255,255)) # set alpha
-        self.images.append(img)
-        self.image = self.images[0]
-        self.rect  = self.image.get_rect()
+        self.screen = screen
+        #self.clock = pygame.time.Clock()
+        #self.current_time = 0.0
+        self.keys = pg.key.get_pressed()   
+        self.done = False                   #конец игры
+        self.fps = 60
+        self.state_dict = {}                #словарь всевозможных состояний
+        self.state_name = None              #имя текущего состояния
+        self.state = None                   #объект класса текущего состояния
+        
+    def setup_states(self, state_dict, start_state):
+        '''инициализация состояний'''
+        self.state_dict = state_dict
+        self.state_name = start_state
+        self.state = self.state_dict[start_state]
 
-'''
-Setup
-'''
+    def update(self):
+        #self.current_time = pygame.time.get_ticks()
+        if self.state.quit:   
+            self.done = True
+        elif self.state.done:
+            self.flip_state()           
+        self.state.update(self.screen, self.keys, self.current_time)
 
-player = Player()   # spawn player
-player.rect.x = 30   # go to x
-player.rect.y = 30   # go to y
-player_list = pygame.sprite.Group()
-player_list.add(player)
+    def flip_state(self):
+        '''смена состояний'''
+        previous, self.state_name = self.state_name, self.state.next
+        persist = self.state.cleanup()
+        self.state = self.state_dict[self.state_name]
+        self.state.startup(self.current_time, persist)
+        self.state.previous = previous
 
-main = True
+    def event_loop(self):
+        for event in pygame.event.get():
+            '''нужно написать обработку событий'''
+            
 
-'''
-Main loop
-'''
-fps   = 40  # frame rate
-ani   = 4   # animation cycles
+    def start():
+        '''mainloop'''
+        while not self.done:
+            self.event_loop()
+            '''и что-то еще'''
+        
 
-while main == True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit(); sys.exit()
-            main = False
+class _State(object):
+    def __init__(self):
+        self.start_time = 0.0
+        self.current_time = 0.0
+        self.done = False
+        self.quit = False
+        self.next = None
+        self.previous = None
+        self.persist = {}
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == ord('q'):
-                pygame.quit()
-                sys.exit()
-                main = False
+    def get_event(self, event):
+        pass
 
-    pygame.display.flip()
-    world.blit(backdrop, backdropbox)
-    player_list.draw(world) # draw player
+    def startup(self, current_time, persistant):
+        self.persist = persistant
+        self.start_time = current_time
+
+    def cleanup(self):
+        self.done = False
+        return self.persist
+
+    def update(self, surface, keys, current_time):
+        pass
+
+
+"""
+#базовый класс состояний (взято у Курячего)
+class GameMode:
+    '''Basic game mode'''
+    def __init__(self):
+        '''Set game mode up
+        - Inittialize black background'''
+        self.background = pygame.Color("black")
+
+    def Events(self,event):
+        '''Event parser'''
+        pass
+
+    def Draw(self, screen):
+        '''Draw game field'''
+        screen.fill(self.background)
+
+    def Logic(self, screen):
+        '''Game logic: what to calculate'''
+        pass
+
+    def Leave(self):
+        '''What to do when leaving this mode'''
+        pass
+
+    def Init(self):
+        '''What to do when entering this mode'''
+        pass
+"""
+
+def main():
+    init()
+    game = Game()
+    states = {} # словаь состояний {"MAIN_MENU": MainMenu()}
+    first_state = "MAIN_MENU"
+    game.setup_states(states, first_state)
+    game.start()
     
-clock.tick(fps)
+if __name__ == '__main__':
+    main()
+    pygame.quit()
+    sys.exit()
